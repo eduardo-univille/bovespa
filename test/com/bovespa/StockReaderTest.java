c7906800b73f49bb026fe0c9943b3d3b2c88d5e1
Eduardo Augusto Klosowski <eduardo.klosowski@univille.br>
2017-03-20 20:41:14 -0300

    Adicionado processamento

--- test/com/bovespa/StockReaderTest.java
+++ test/com/bovespa/StockReaderTest.java
@@ -1,5 +1,8 @@
 package com.bovespa;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
 import org.junit.Test;
 
 public class StockReaderTest {
@@ -10,5 +13,35 @@ public class StockReaderTest {
         for (Stock stock : stocks) {
             System.out.println(stock.getCode());
         }
+    }
+
+    @Test
+    public void readPetrobrasTest() throws Exception {
+        StockReader stockReader = new StockReader();
+        Stock[] stocks = stockReader.read("COTAHIST_A2016.TXT");
+        for (Stock stock : stocks) {
+            LocalDate date = stock.getDate();
+            if (date.getYear() == 2016 && date.getMonthValue() == 3 && date.getDayOfMonth() == 21
+                    && stock.getCode().equals("PETR4")) {
+                System.out.println(stock.getOpenPrice());
+                break;
+            }
+        }
+    }
+
+    @Test
+    public void readValeTest() throws Exception {
+        StockReader stockReader = new StockReader();
+        Stock[] stocks = stockReader.read("COTAHIST_A2016.TXT");
+        int days = 0;
+        BigDecimal result = BigDecimal.ZERO;
+        for (Stock stock : stocks) {
+            LocalDate date = stock.getDate();
+            if (stock.getCode().equals("VALE5") && date.getYear() == 2016 && date.getMonthValue() == 4) {
+                days++;
+                result = result.add(stock.getClosePrice());
+            }
+        }
+        System.out.println(result.divide(BigDecimal.valueOf(days)));
     }
 }
