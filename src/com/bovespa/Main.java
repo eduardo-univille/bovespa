5c845c882bb088576ae2ffd529de0ce3dbdc3816
Eduardo Augusto Klosowski <eduardo.klosowski@univille.br>
2017-03-20 19:40:19 -0300

    Parser feito

--- src/com/bovespa/Main.java
+++ src/com/bovespa/Main.java
@@ -1,5 +1,6 @@
 package com.bovespa;
 
+import java.math.BigDecimal;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.function.Consumer;
@@ -20,8 +21,16 @@ public class Main {
                     Stock stock = new Stock();
                     stock.setCode(line.substring(12, 24).trim());
                     stock.setDate(LocalDate.parse(line.substring(2, 10), DateTimeFormatter.ofPattern("yyyyMMdd")));
+                    stock.setOpenPrice(new BigDecimal(line.substring(56, 67) + "." + line.substring(67, 69)));
+                    stock.setClosePrice(new BigDecimal(line.substring(108, 119) + "." + line.substring(119, 121)));
+                    stock.setMaxPrice(new BigDecimal(line.substring(69, 80) + "." + line.substring(80, 82)));
+                    stock.setMinPrice(new BigDecimal(line.substring(82, 93) + "." + line.substring(93, 95)));
                     System.out.println(stock.getCode());
                     System.out.println(stock.getDate());
+                    System.out.println(stock.getOpenPrice());
+                    System.out.println(stock.getClosePrice());
+                    System.out.println(stock.getMaxPrice());
+                    System.out.println(stock.getMinPrice());
                     stocks[i++] = stock;
                 }
             }
